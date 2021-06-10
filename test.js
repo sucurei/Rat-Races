a2154b781b3c4c0c966a8dbbae9f338efb8d0616
sucurei <62082711+sucurei@users.noreply.github.com>
2021-06-10 12:34:08 +0300

    Finalul
    Finalul

--- test.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://123:123@cluster0.ru79j.mongodb.net/db?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
-console.log("caca");
